c766180f1e4c39a9ff502a94b84dae3f7bba28f4
zach1502 <zach1502@users.noreply.github.com>
2018-09-01 00:09:32 -0700

    Update guess.py

--- cogs/guess.py
+++ cogs/guess.py
@@ -1,12 +1,8 @@
 """importsss (that i don't know if it belongs here)
 
     you can get rid of excess comments"""
-import time
-import random
 from discord.ext import commands
-from random import choice
-from collections import Counter, defaultdict, namedtuple
-import discord
+import random
 import os
 import asyncio
 import re
@@ -15,7 +11,7 @@ class Guess:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="guess", pass_context = True)
+    @commands.command(name="guess", pass_context=True)
     async def mycom(self):
         # welcoming the user
         await self.bot.reply("guess what word I'm thinking of!")
@@ -32,18 +28,18 @@ class Guess:
         secret = "its gonna change"
 
         # Probably need to create a word list and grab words from there
-        items = ["here", "are", "some", "strings", "of", "which", "we", "will", 
-         "select", "one", "two", "three", "four", "five", "six", 
-         "seven", "eight", "nine", "ten", "python", "ruby", "java", "javascript", "traceback", "print", 
-         "random", "input", "raw", "print", "sleep", "time", "secret", "while", "then", "if", 
-         "loop", "in", "else", "break", "empty", "mocha", "matcha", "money", "spam","jazz","jam","spaz", 
-         "binder", "jokes", "chemistry", "person", "matcha", "night", "everyone", "pasta", "craving", 
-         "jolly", "ranch", "lit", "still", "first", "second", "third", "skull", "ketchup", 
-         "ice", "red", "life", "death", "magic", "comments", "class", "secret", "value", 
-         "variable", "guess", "incorrect", "russian", "roulette", "bot", "turn", 
-         "failed", "wood", "stone", "cobblestone", "blanket", "ore", "chest", "poster", "hash", "secure", "magical", "never", "public",
-         "private", "key", "encryption", "cypher", "cryptogram", "theory", "open", "thread", "ripper", "core", "graphical", "graphics",
-         "asymmetrical", "symmetrical"]
+        items = ["here", "are", "some", "strings", "of", "which", "we", "will",
+                 "select", "one", "two", "three", "four", "five", "six",
+                 "seven", "eight", "nine", "ten", "python", "ruby", "java", "javascript", "traceback", "print",
+                 "random", "input", "raw", "print", "sleep", "time", "secret", "while", "then", "if", 
+                 "loop", "in", "else", "break", "empty", "mocha", "matcha", "money", "spam", "jazz", "jam",
+                 "spaz", "binder", "jokes", "chemistry", "person", "matcha", "night", "everyone", "pasta", "craving",
+                 "jolly", "ranch", "lit", "still", "first", "second", "third", "skull", "ketchup",
+                 "ice", "red", "life", "death", "magic", "comments", "class", "secret", "value",
+                 "variable", "guess", "incorrect", "russian", "roulette", "bot", "turn",
+                 "failed", "wood", "stone", "cobblestone", "blanket", "ore", "chest", "poster", "hash",
+                 "secure", "magical", "never", "public", "private", "key", "encryption", "cypher", "cryptogram", "theory",
+                 "open", "thread", "ripper", "core", "graphical", "graphics", "asymmetrical", "symmetrical"]
         # picks one word from list
         secret = random.choice(items)
 
@@ -62,14 +58,11 @@ class Guess:
                 # Win text
             if failed == 0:
                 await self.bot.say("You won! Good job! :aquaThumbsUp:")  
-                # exit the script"""
-            
+                # exit the script"""      
                 break
             # ask the user go guess a character"""
             await self.bot.say("lower case letters please~~!")
-            guess = self.bot.discord.wait_for_message(channel = context.message.channel)
-            
-            
+            guess = self.bot.wait_for_message(channel = context.message.channel)
             while guess == None:
                 await asyncio.sleep(30)
                 await self.bot.say("I'm just going to assume you're gone...You think too slowly")
