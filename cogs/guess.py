8244361e1980ae4b99677ce1bea7c9bd18261dbe
zach1502 <zach1502@users.noreply.github.com>
2018-09-01 00:39:08 -0700

    Update guess.py

--- cogs/guess.py
+++ cogs/guess.py
@@ -1,18 +1,20 @@
 """importsss (that i don't know if it belongs here)
 
     you can get rid of excess comments"""
-from discord.ext import commands
 import random
 import os
 import asyncio
 import re
-"""Guessing game """
+from discord.ext import commands
+
 class Guess:
+    '''this is guessing game'''
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="guess", pass_context=True)
     async def mycom(self):
+        '''pylint told me to do this'''
         # welcoming the user
         await self.bot.reply("guess what word I'm thinking of!")
 
@@ -28,18 +30,19 @@ class Guess:
         secret = "its gonna change"
 
         # Probably need to create a word list and grab words from there
-        items = ["here", "are", "some", "strings", "of", "which", "we", "will",
-                 "select", "one", "two", "three", "four", "five", "six",
-                 "seven", "eight", "nine", "ten", "python", "ruby", "java", "javascript", "traceback", "print",
-                 "random", "input", "raw", "print", "sleep", "time", "secret", "while", "then", "if", 
-                 "loop", "in", "else", "break", "empty", "mocha", "matcha", "money", "spam", "jazz", "jam",
-                 "spaz", "binder", "jokes", "chemistry", "person", "matcha", "night", "everyone", "pasta", "craving",
-                 "jolly", "ranch", "lit", "still", "first", "second", "third", "skull", "ketchup",
-                 "ice", "red", "life", "death", "magic", "comments", "class", "secret", "value",
-                 "variable", "guess", "incorrect", "russian", "roulette", "bot", "turn",
-                 "failed", "wood", "stone", "cobblestone", "blanket", "ore", "chest", "poster", "hash",
-                 "secure", "magical", "never", "public", "private", "key", "encryption", "cypher", "cryptogram", "theory",
-                 "open", "thread", "ripper", "core", "graphical", "graphics", "asymmetrical", "symmetrical"]
+        items = ["here", "are", "some", "strings", "of", "which", "we", "will", "select", "one",
+                 "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "python",
+                 "ruby", "java", "javascript", "traceback", "print", "random", "input", "raw", 
+                 "print", "sleep", "time", "secret", "while", "then", "if", "loop", "in", "else", 
+                 "break", "empty", "mocha", "matcha", "money", "spam", "jazz", "jam", "spaz", 
+                 "binder", "jokes", "chemistry", "person", "matcha", "night", "everyone", "pasta",
+                 "craving", "jolly", "ranch", "lit", "still", "first", "second", "third", "skull", 
+                 "ketchup","ice", "red", "life", "death", "magic", "comments", "class", "secret", 
+                 "value", "variable", "guess", "incorrect", "russian", "roulette", "bot", "turn",
+                 "failed", "wood", "stone", "cobblestone", "blanket", "ore", "chest", "poster",
+                 "hash", "secure", "magical", "never", "public", "private", "key", "encryption",
+                 "cypher", "cryptogram", "theory", "open", "thread", "ripper", "core", "graphical",
+                 "graphics", "asymmetrical", "symmetrical"]
         # picks one word from list
         secret = random.choice(items)
 
@@ -50,30 +53,29 @@ class Guess:
             failed = 0
             for char in secret:
                 if char in guesses:
-                    await self.bot.say(char),
+                    await self.bot.say(char)
                 else:
-                    await self.bot.say("_"),
+                    await self.bot.say("_")
                     failed += 1
                 #if failed is equal to zero
                 # Win text
             if failed == 0:
-                await self.bot.say("You won! Good job! :aquaThumbsUp:")  
+                await self.bot.say("You won! Good job! :aquaThumbsUp:")
                 # exit the script"""      
                 break
             # ask the user go guess a character"""
             await self.bot.say("lower case letters please~~!")
-            guess = self.bot.wait_for_message(channel = context.message.channel)
-            while guess == None:
+            guess = self.bot.wait_for_message(channel=context.message.channel)
+            while not guess:
                 await asyncio.sleep(30)
                 await self.bot.say("I'm just going to assume you're gone...You think too slowly")
                 break
-                
             # add to the player(s) guess to guesses"""
             guesses += guess
             # incorrect letter"""
             if guess not in secret:
-                turns -= 1        
-                await self.bot.say("Wrong") 
+                turns -= 1
+                await self.bot.say("Wrong")
                 #if the turns equal to zero
                 if turns == 0:
                     # losing text
@@ -84,13 +86,10 @@ class Guess:
                     await self.bot.say("You have {0} more guesses".format(turns))
                     
                     
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# # # Set-up # # #     # # # Set-up # # #    # # # Set-up # # #    # # # Set-up # # #    # # # Set-up # # #    # # # Set-up # # #    # # # Set-up # # #    # # # Set-up # # #                           å∫ç∂´ƒ©˙ˆ∆˚¬µ˜øπœ¨®ß†¨√∑≈¥Ω å∫ç∂´ƒ©˙ˆ∆˚¬µ˜øπœ¨®ß†¨√∑≈¥Ω å∫ç∂´ƒ©˙ˆ∆˚¬µ˜øπœ¨®ß†¨√∑≈¥Ω å∫ç∂´ƒ©˙ˆ
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
+# # # Set-up # # #     # # # Set-up # # #    # # # Set-up # # #    # # # Set-up # # #    # # # Set-up
+# --------------------------------------------------------------------------------------------------
 def setup(bot):
-    check_folders()
-    check_files()
     bot.add_cog(Guess(bot))
 
-    
-# dab xd oof indents 
+# dab xd oof indents
