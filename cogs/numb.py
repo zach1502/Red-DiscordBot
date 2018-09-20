59f70ce65f5389a0cdb541803c1e258fbf33c704
zach1502 <zach1502@users.noreply.github.com>
2018-09-20 14:42:07 -0700

    Update numb.py

--- cogs/numb.py
+++ cogs/numb.py
@@ -15,8 +15,8 @@ class NumbGuess:
 	    GuessesTaken = 0
 	    await self.bot.say("how hard would you like the number game to be? (easy/mid/hard)")
 	    mode = self.bot.wait_for_message(channel=context.message.channel, timeout=30)
-	    if mode == 'easy':
-		number = random.randint(1, 10)
+            if mode == 'easy':
+                number = random.randint(1, 10)
 
 	    elif mode == 'mid':
 		number = random.randint(1,50)
