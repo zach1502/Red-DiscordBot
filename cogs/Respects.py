b5c016bc70e0ac1a3690569a1ba2012b9f37918b
zach1502 <zach1502@users.noreply.github.com>
2018-11-05 14:50:33 -0800

    Some changes suggested by Pylint
    Fekkin Pylint

--- cogs/Respects.py
+++ cogs/Respects.py
@@ -1,23 +1,22 @@
 '''Press "F" to pay respects.'''
 import asyncio
-import discord
 import random
 
 
 class Respects:
     '''Press "F" to pay respects.'''
-    def __init__(self, bot, argument):
+    def __init__(self, bot):
         self.bot = bot
     @commands.command(name="f", pass_context=True) # pylint-disable=too-few-public-methods
     async def mycom(self, ctx):
         '''Press "F" to pay respects.'''
-        Who_F = []
-        Who_F.append(ctx)
+        who_F = []
+        who_F.append(ctx + ", ")
         hearts = ["💚", "💙", "💜", "💘", "🧡", "♥️", "💓", "💖", "💕", "💛", "💗", "💞", "💝", "❤️", "🖤"]
-        await bot.say(Who_F + "has paid their respects " + random.choice(hearts))
+        await bot.say(who_F + "has paid their respects " + random.choice(hearts))
 
         await asyncio.sleep(10)
-        Who_F.clear()
+        who_F.clear()
 
 # Set Down
 def setup(bot):
