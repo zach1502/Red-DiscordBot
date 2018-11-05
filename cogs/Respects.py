9023e02936628c28e8f07fd3124f8e0bfd24a405
zach1502 <zach1502@users.noreply.github.com>
2018-11-05 14:02:20 -0800

    Respects.py finished
    I think it works.
    Travis will tell

--- cogs/Respects.py
+++ cogs/Respects.py
@@ -1,16 +1,25 @@
-'''Press "F" to pay respects'''
+'''Press "F" to pay respects.'''
 import asyncio
 import discord
 import random
-import logging
 
 
 class Respects:
-    '''Press "F" to pay respects'''
+    '''Press "F" to pay respects.'''
     def __init__(self, bot, argument):
         self.bot = bot
     @commands.command(name="f", pass_context=True) # pylint-disable=too-few-public-methods
-    async def mycom(self, ctx, member : discord.Member):
-        '''Press "F" to pay respects'''
+    async def mycom(self, ctx):
+        '''Press "F" to pay respects.'''
+        Who_F = []
+        Who_F.append(ctx)
         hearts = ["💚", "💙", "💜", "💘", "🧡", "♥️", "💓", "💖", "💕", "💛", "💗", "💞", "💝", "❤️", "🖤"]
-        await bot.say({0} + "has paid their respects " + random.choice(hearts).format(ctx))
+        await bot.say(Who_F + "has paid their respects " + random.choice(hearts))
+
+        await asyncio.sleep(10)
+        Who_F.clear()
+
+# Set Down
+def setup(bot):
+    '''Setup.'''
+    bot.add_cog(Respects(bot))
