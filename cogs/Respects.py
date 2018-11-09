4cde9f4be913727a19bbbe6aa85e00778b92b65b
zach1502 <zach1502@users.noreply.github.com>
2018-11-09 13:36:38 -0800

    create f(x)
    its a WIP

--- cogs/Respects.py
+++ cogs/Respects.py
@@ -3,19 +3,31 @@ import asyncio
 import random
 
 
-class Respects:
+class Respects: # pylint-disable=too-few-public-methods
     '''Press "F" to pay respects.'''
     def __init__(self, bot):
         self.bot = bot
     @commands.command(name="f", pass_context=True) # pylint-disable=too-few-public-methods
     async def mycom(self, ctx):
         '''Press "F" to pay respects.'''
-        who_F = []
-        who_F.append(ctx + ", ")
+        who_F = [] # pylint-disable=invalid-name
+        who_F.append(ctx.message.author + ", ")
         hearts = ["💚", "💙", "💜", "💘", "🧡", "♥️", "💓", "💖", "💕", "💛", "💗", "💞", "💝", "❤️", "🖤"]
-        await bot.say(who_F + "has paid their respects " + random.choice(hearts))
-
-        await asyncio.sleep(10)
+        await bot.say(who_F + "has paid their respects " + random.choice(hearts)) # pylint-disable=undefined-variable
+    
+    async def Helper(self, ctx):
+        '''Apu Apustaja checks when latest invocation was'''
+        
+        
+        #we`ll probably want a helper function to do some kind of a lookup to see when the latest invocation was
+        #and if it exceeds, then we want a new "has paid respects", 
+       #else append to the old one, delete the pre-existing "respects", 
+        #and put a new one.
+    
+    
+    
+    
+    
         who_F.clear()
 
 # Set Down
